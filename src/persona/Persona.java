e67bb4b82f656dec891b72664268a8af2298da25
Alejandro Laredo <a_alejandro.laredo.de.los.reyes@iespablopicasso.es>
2023-11-29 09:49:48 +0100

    Añado persona

--- src/persona/Persona.java
+++ src/persona/Persona.java
@@ -1,5 +1,20 @@
 package persona;
 
 public class Persona {
-
+  
+  private String nombre;
+  private String apellidos;
+  private int edad;
+  
+  Persona(String nombre, String apellidos, int edad){
+    this.nombre = nombre;
+    this.apellidos = apellidos;
+    this.edad = edad;
+  }
+  
+  public int Cumpleaños() {
+    edad++;
+    System.out.println("La edad es de " + edad);
+    return edad;
+  }
 }
